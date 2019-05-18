c7283c2781fbac284938835d5d51c5bb29931010
pjd <rovyko@protonmail.com>
2019-05-18 13:09:11 -0400

    bump version

--- codepentabs.user.js
+++ codepentabs.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           Codepen Tabs
-// @version        0.2019.05.18.2
+// @version        0.2019.05.18.3
 // @author         rovyko
 // @namespace      https://github.com/rovyko/codepen-tabs
 // @downloadURL    https://github.com/rovyko/codepen-tabs/raw/master/codepentabs.user.js
